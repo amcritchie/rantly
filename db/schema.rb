973f2819611552e5239ead07460654640a244db5
Alex McRitchie <amcritchie@gmail.com>
2014-10-07 11:15:10 -0600

    about to fetch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141004063140) do
+ActiveRecord::Schema.define(version: 20141006222436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20141004063140) do
     t.string   "last_name"
     t.string   "bio"
     t.string   "rant_frequency"
+    t.string   "image_url"
   end
 
 end
