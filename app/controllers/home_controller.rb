863c8137fef491bbf1b246bfcf383abb30ea289b
Alex McRitchie <amcritchie@gmail.com>
2014-10-12 22:48:41 -0600

    my rants and last ranrs css looks good

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     @rant = Rant.new
+    @my_rants = Rant.where(user_id: current_user).reverse.first(2)
+    @latest_rants = Rant.all.reverse.first(2)
   end
 end
