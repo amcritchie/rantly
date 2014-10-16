5544fd7fadc0b75552f607deedd3c643d15b8d88
Alex McRitchie <amcritchie@gmail.com>
2014-10-15 18:48:35 -0600

    dashboard rants show a maximum amount of characters.

--- app/models/rant.rb
+++ app/models/rant.rb
@@ -1,7 +1,7 @@
 class Rant < ActiveRecord::Base
   belongs_to :user
   has_many :favorites
-  validates :rant, length: {minimum: 140, maximum: 800}
+  validates :rant, length: {minimum: 140}
   validates :rant, presence: true
   validates :title, presence: true
 end
